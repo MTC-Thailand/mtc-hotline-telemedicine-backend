519ee1c3b3ea7339e939d701f88602374a7262d3
Likit Preeyanon <likit.pre@mahidol.edu>
2022-03-04 23:49:43 +0700

    initial commit with a view for sending a text message to Line

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, request, jsonify
+from linebot import LineBotApi
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+
+@app.route('/book-confirm')
+def send_booking_confirmation():
+    payload = request.json
+    if payload['lineId']:
+        line_bot_api.push_message(payload['lineId'], TextSendMessage(text=payload['message']))
+        return jsonify(['message', 'success'])
+    else:
+        return jsonify(['message', 'failure'])
